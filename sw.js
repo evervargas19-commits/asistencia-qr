75fe9e9c1b3a6106c05b102c8f888346f51dde76
evervargas19-commits <evervargas19@gmail.com>
2026-09-09 00:30:15 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'asistencia-qr-v2';
+const ARCHIVOS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './html5-qrcode.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ARCHIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((nombres) =>
+      Promise.all(
+        nombres.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cacheado) => {
+      return cacheado || fetch(event.request);
+    })
+  );
+});
